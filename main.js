46f2d93401974c74df1f01f1b9d34ab22648645e
SophieTonnoir <laptitesophie78@hotmail.fr>
2016-10-22 17:31:50 -0400

    compteur fini

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
 $(function() {
 
-// Code à implémenter
+    // evenement appliqué au click de la souris sur le bouton d'ID countButton
+	$("#countButton").click(function(){
+        //on démarre le worker et l'execution du programme
+		startWorker();
+	});
+    // evenement appliqué au click de la souris sur le bouton d'ID cancelButton
+	$("#cancelButton").click(function(){
+		stopWorker();
+	});
 
 });
+
+var w;
+//fonction pour démarrer le worker et l'execution du programme
+function startWorker() {
+    
+    if(typeof(Worker) !== "undefined") {
+
+        if(typeof(w) == "undefined") {
+            //on initialise le worker
+            w = new Worker("worker.js");
+        }
+        // on envoie un message au worker.js avec le contenu du champ texte
+        w.postMessage(['start',$('#comment').val()]); 
+       
+       //on reçoit le message du worker et on change les élements HTML correspondant
+        w.onmessage = function(event){
+			 $('#result').html(event.data[0]);
+			 $('#progressBar').attr('style','width:'+event.data[1]+'%');
+			 $('#progressBar').text(event.data[1]+'%'); 
+        };
+       
+    } 
+}
+// fonction pour stopper l'execution du programme
+function stopWorker() { 
+    w.terminate();
+    $('#result').html("Annulé");
+    w = undefined;
+}
--- worker.js
+++ worker.js
@@ -1,11 +1,23 @@
-
+//on importe le fichier CompteurJetons.js pour pouvoir utiliser les fonctions qui s'y trouvent
 self.importScripts("CompteurJetons.js");
 var compteur = new CompteurJetons();
 
 self.addEventListener('message', function(e) {
-	port.onmessage = function(){
-		var workerResult = 'Result: ' + (compteur.getJeton() * compteur.getProcess();
-	    	port.postMessage(workerResult);
+	if(e.data[0] == 'start'){
+
+		//on execute la fonction compterJetons()
+		var words = compteur.compterJetons(e.data[1]);
+		var nb = words.next().value;
+		
+		/**
+		* on parcourt le resultat de la fonction compterJeton() et pour chaque itération on envoie un message
+		* contenant le pourcentage de progression et le nombre de jetons actuel.
+		* On envoie ce message pour que l'on puisse modifier le contenu des éléments HTML correspondant
+		*/
+		while(nb !== undefined){
+			postMessage([compteur.getJetons(),nb]);
+			nb = words.next().value;
+		}
 	}
   
 }, false);
